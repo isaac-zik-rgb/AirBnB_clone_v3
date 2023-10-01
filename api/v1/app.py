af8e6377e9894e4f833c201a097fb3894d7fafb8
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-10-01 13:50:13 +0100

    error Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,7 +21,6 @@ def error_handler(error):
     return jsonify({"error": "Not found"}), 404
 
 
-
 if __name__ == "__main__":
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
     port = int(os.environ.get("HBNB_API_PORT", 5000))
