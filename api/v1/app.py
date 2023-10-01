9d18e1b1d3b1495b9384aa2426cf52bcc9562d37
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-10-01 14:06:37 +0100

    error Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ initaite a flask web application"""
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 import os
-from flask import jsonify
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -19,7 +19,7 @@ def close_db(exc):
 @app.errorhandler(404)
 def error_handler(error):
     """ return a jsonn formated error"""
-    return jsonify({"error": "Not found"}), 404
+    return make_response(({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
