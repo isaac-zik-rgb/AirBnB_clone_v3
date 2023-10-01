13d3d50293e4640eaf03b08176978a4ef7582c21
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-10-01 13:03:58 +0100

    count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,28 @@
 """ define our route functions"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route("/status")
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status():
     """returns a JSON status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def retrive_count():
+    """ an enpoint that retrives the number of each objects"""
+    stats = {}
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+    for i in len(classes):
+        stats[names[i]] = storage.count(classes[i])
+
+    return jsonify(stats)
